884234d501b8d1d8980afb0de5f2e4f9549316ea
Juliana Vitoria <julianaenob@gmail.com>
2026-07-20 12:21:05 -0300

    feat: implementa Model, Repository e Controller do recurso Tema

--- src/main/java/com/generation/blogpessoall/model/Postagem.java
+++ src/main/java/com/generation/blogpessoall/model/Postagem.java
@@ -18,7 +18,7 @@ import jakarta.validation.constraints.Size;
 
 public class Postagem {
 	
-	@Id  
+	@Id 
 	@GeneratedValue(strategy = GenerationType.IDENTITY) // AUTO_INCREMENT
 	private Long id;
 	

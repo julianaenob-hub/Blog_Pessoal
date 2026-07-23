cace39beaf8c539262501a0bc84ab62bd059c997
Juliana Vitoria <julianaenob@gmail.com>
2026-07-23 16:08:15 -0300

    Implementa Security

---
+++ src/main/java/com/generation/blogpessoall/repository/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.generation.blogpessoall.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.blogpessoall.model.Usuario;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
+
+	Optional<Usuario> findByUsuario(String usuario);
+
+}

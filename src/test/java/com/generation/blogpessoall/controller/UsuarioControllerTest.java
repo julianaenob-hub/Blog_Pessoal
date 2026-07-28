775cca645692c171092c6f57c8240078aba27e78
Juliana Vitoria <julianaenob@gmail.com>
2026-07-28 12:16:04 -0300

    Adiciona Testes

---
+++ src/test/java/com/generation/blogpessoall/controller/UsuarioControllerTest.java
@@ -0,0 +1,192 @@
+package com.generation.blogpessoall.controller;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.TestRestTemplate;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureTestRestTemplate;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.generation.blogpessoall.model.Usuario;
+import com.generation.blogpessoall.repository.UsuarioRepository;
+import com.generation.blogpessoall.service.UsuarioService;
+import com.generation.blogpessoall.util.JwtHelper;
+import com.generation.blogpessoall.util.TestBuilder;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureTestRestTemplate
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@TestMethodOrder(MethodOrderer.DisplayName.class)
+public class UsuarioControllerTest {
+	
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	private static final String BASE_URL = "/usuarios";
+	private static final String USUARIO = "/root@root.com";
+	private static final String SENHA = "/rootroot";
+	
+	@BeforeAll
+	void inicio () {
+		usuarioRepository.deleteAll();
+		usuarioService.cadastrarUsuario(TestBuilder.criarUsuario(null, "Root", USUARIO, SENHA));
+	}
+
+	@Test
+	@DisplayName("01 - Deve cadastrar um novo usuario com sucesso")
+	void deveCadastrarUsuario() {
+		//Given
+		Usuario usuario = TestBuilder.criarUsuario(null, "Gabrielle Guimaraes",
+				"gabrielle@email.com.br", "gabi1234");
+		
+		//When
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuario);
+		
+		//Enviar a Requisicao
+		ResponseEntity<Usuario> resposta = testRestTemplate.exchange(
+				BASE_URL + "/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+		
+		
+		//Then
+		
+		assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+		assertNotNull(resposta.getBody());
+		
+	}
+	
+	
+	
+	@Test
+	@DisplayName("02 - Não Deve Cadastrar um usuário Duplicado")
+	void naoDeveCadastrarUsuarioDuplicdo(){
+		
+		// given 
+		Usuario usuario = TestBuilder.criarUsuario(null, "Luiza Guimarães",
+				"luizagui@email.com", "lugui12345678");
+		
+		
+		// when 
+		// Corpo da Requisição
+		HttpEntity<Usuario> corpoRequisicao = new HttpEntity<Usuario>(usuario);
+		
+		// Enviar a Requisição
+		ResponseEntity<Usuario> resposta = testRestTemplate.exchange(
+				BASE_URL + "/cadastrar", HttpMethod.POST, corpoRequisicao, Usuario.class);
+		
+		// then 
+		
+		assertEquals(HttpStatus.CREATED, resposta.getStatusCode());
+		assertNotNull(resposta.getBody());
+		
+	}
+	
+	@Test
+	@DisplayName("03 - Deve listar todos os usuarios")
+	void deveListarTodosUsuarios(){
+		
+		// given 
+		usuarioService.cadastrarUsuario(TestBuilder.criarUsuario(null, "Kaue Dota", "kaue@email.com", "kaue1234"));
+		usuarioService.cadastrarUsuario(TestBuilder.criarUsuario(null, "Edson Nascimento", "edson@email.com", "edson1234"));
+		// when 
+		
+		String token = JwtHelper.obterToken(testRestTemplate, USUARIO, SENHA);
+		// Cabeçalho da Requisição
+		HttpEntity<Void> cabecalhoRequisicao = JwtHelper.criarRequisicaoComToken(token);
+		
+		// Enviar a Requisição
+		ResponseEntity<Usuario[]> resposta = testRestTemplate.exchange(
+				BASE_URL + "/all", HttpMethod.GET, cabecalhoRequisicao, Usuario[].class);
+		
+		// then 
+		
+		assertEquals(HttpStatus.OK, resposta.getStatusCode());
+		assertNotNull(resposta.getBody());
+		
+	}
+	@Test
+	@DisplayName("04 - Deve Atualizar os dados do usuário com sucesso")
+	void deveAtualizarUsuario() {
+		// Given
+ 
+		// Objeto para fazer o cadastro
+		Usuario usuario = TestBuilder.criarUsuario(null, "Daniel", "daniel@email.com.br", "daniel1234");
+ 
+		// Fiz o cadastro e guardei os dados objeto
+		Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(usuario);
+ 
+		// Preparar o objeto com a atualização
+		Usuario usuarioUpdate = TestBuilder.criarUsuario(usuarioCadastrado.get().getId(), "Daniel Araujo",
+				"daniel_araujo@email.com.br", "abcd1234");
+ 
+		// When
+ 
+		// Obter o Token
+		String token = JwtHelper.obterToken(testRestTemplate, USUARIO, SENHA);
+ 
+		// Cabeçalho da Requisição
+		HttpEntity<Usuario> cabecalhoRequisicao = JwtHelper.criarRequisicaoComToken(usuarioUpdate, token);
+ 
+		
+		// Enviar a Requisição
+		ResponseEntity<Usuario> resposta = testRestTemplate.exchange(BASE_URL + "/atualizar", HttpMethod.PUT,
+				cabecalhoRequisicao, Usuario.class);
+ 
+		// Then
+ 
+		assertEquals(HttpStatus.OK, resposta.getStatusCode());
+		assertNotNull(resposta.getBody());
+ 
+	}
+			
+	@Test
+	@DisplayName("05 - Deve listar um usuário por ID")
+	void deveListarUsuarioPorId() {
+
+	    // Given
+	    Usuario usuario = TestBuilder.criarUsuario(null, "Juliana",
+	            "juliana@email.com.br", "juliana1234");
+
+	    Optional<Usuario> usuarioCadastrado = usuarioService.cadastrarUsuario(usuario);
+
+	    // When
+
+	    // Obter o Token
+	    String token = JwtHelper.obterToken(testRestTemplate, USUARIO, SENHA);
+
+	    // Cabeçalho da Requisição
+	    HttpEntity<Void> cabecalhoRequisicao = JwtHelper.criarRequisicaoComToken(token);
+
+	    // Enviar a Requisição
+	    ResponseEntity<Usuario> resposta = testRestTemplate.exchange(
+	            BASE_URL + "/" + usuarioCadastrado.get().getId(),
+	            HttpMethod.GET,
+	            cabecalhoRequisicao,
+	            Usuario.class);
+
+	    // Then
+	    assertEquals(HttpStatus.OK, resposta.getStatusCode());
+	    assertNotNull(resposta.getBody());
+	}
+	
+}
+
+
